d5a0315d0fc633eda1a54bc2625bd20fa9f4498d
aalimd <50314384+aalimd@users.noreply.github.com>
2026-05-18 15:05:40 +0300

    تعديل المسار الأساسي ليتوافق مع المجلد الفرعي Ejazat

--- includes/config.php
+++ includes/config.php
@@ -26,7 +26,8 @@ if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1'
     define('DB_NAME', '********_ejazat');
     define('DB_USER', '********_ejazatuser');
     define('DB_PASS', '********');
-    define('BASE_URL', 'https://ejazat.aalimd.com/');
+    // إذا كان التطبيق داخل مجلد Ejazat
+    define('BASE_URL', '/Ejazat/'); 
 }
 
 // بدء الجلسة
